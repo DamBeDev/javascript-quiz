93be47c48bc4dc25baad910ec253e62259efaf9b
DamBeDev <143622448+DamBeDev@users.noreply.github.com>
2025-04-09 11:07:52 -0300

    feat: Configure basic page

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { Container, Stack, Typography } from '@mui/material'
+import { JavaScriptLogo } from './JavaScriptLogo'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+function App() {
+
+  return (
+    <>
+      <Container maxWidth='sm'>
+
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
+          <JavaScriptLogo />
+          <Typography variant='h2' component='h1'>
+            JavaScript Quizz
+          </Typography>
+        </Stack>
+
+      </Container>
+    </>
+  )
+}
+
+export default App
